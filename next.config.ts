f5017aa1b3f71967f2733f17f28a575a8d307f0d
LaneLR <laneleerichardson@gmail.com>
2026-03-03 17:26:33 -0600

    push docs folder

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: 'export',
+  distDir: 'docs', 
   images: {
-    unoptimized: false,
+    unoptimized: true,
   },
 };
 
