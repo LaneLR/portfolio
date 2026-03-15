da7a10db2a26938b958bcd63e08b21eaf6ed87ae
LaneLR <laneleerichardson@gmail.com>
2026-03-14 23:00:07 -0500

    update experiences

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -5,17 +5,25 @@ const EXPERIENCES = [
   {
     company: "Verizon",
     role: "Software Engineer I",
-    period: "2025 - Present",
+    period: "March 2025 - Present",
     description:
-      "Modernizing high-traffic webpages for leading telecommunications brands, including Total Wireless and Visible, optimizing frontend performance, user experience, and accessibility for millions of users.",
-    skills: ["React", "Next.js", "TypeScript", "JavaScript", "HTML", "SCSS", "Redux", "Figma", "Jira", "Jenkins", "SonarQube",],
+      "Optimizing webpages for telecommunications brands like Total Wireless, Straight Talk, and Visible. Primarily focused on frontend performance and SEO, and improving user experience for millions of customers.",
+    skills: ["React", "Next.js", "TypeScript", "JavaScript", "HTML", "SCSS", "Redux", "Figma", "Jira", "Jenkins", "SonarQube", "Java"],
   },
   {
     company: "",
-    role: "Independent Full-stack Developer",
+    role: "Multiverse Applied Full Stack Software Engineering Program",
+    period: "March 2025 - March 2026",
+    description:
+      "A full-stack bootcamp that improved my skills. This program began as a 3-month full-stack bootcamp and the last 9 months were spent developing my backend skills (Java Spring Boot, Node.js, Express), refining my frontend skills (React, Next.js, TypeScript), and deepening my understanding of deployment processes and related tools (SonarQube, Github Actions).",
+    skills: ["Java", "Spring Boot", "React", "Next.js", "Node.js", "Express", "JavaScript", "HTML", "CSS", "GitHub Actions", "SonarQube",],
+  },
+  {
+    company: "",
+    role: "Independent Developer",
     period: "2023 - Present",
     description:
-      "Developing a variety of projects, including an AI image analysis mobile app, a calendar and scheduling application, and a news aggregator service.",
+      "Currently building agentic AI agents for apps that are still in development. Some of my other work includes building an item reselling mobile app that serves as an inventory manager and utilzizes AI for price comparison, a fully functional calendar app to make plans with friends, and a news aggregator service to find all of your favorite news sources in one place.",
     skills: ["Java", "Python", "React", "Next.js", "Node.js", "Express", "TypeScript", "JavaScript", "HTML", "SCSS", "Stripe", "Capacitor", "Vertex AI", "Gemini API", "GitHub Actions", "SonarQube",],
   },
 ];
