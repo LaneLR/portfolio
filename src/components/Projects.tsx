ca1467c4684663236ee64d5529d0e17a592a9be6
LaneLR <laneleerichardson@gmail.com>
2026-03-14 22:24:34 -0500

    add deployed links and repolinks to each project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,42 +2,52 @@
 "use client";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import { Link2, LinkIcon } from "lucide-react";
 
 const PROJECTS = [
   {
     id: "resale-iq",
     title: "AI Image Analyzer Mobile App",
-    description: "AI image analysis mobile application for item identification, valuation, and inventory management.",
+    description:
+      "AI image analysis mobile application for item identification, valuation, and inventory management.",
     image: `/images/MultiPageDisplay.png`,
+    repoLink: `projects/ai-analyzer`,
     link: `projects/ai-analyzer`,
   },
   {
     id: "planz",
     title: "Planz Calendar App",
-    description: "Calendar and scheduling app for creating and tracking events with friends.",
+    description:
+      "Calendar and scheduling app for creating and tracking events with friends.",
     image: `/images/planzdisplay.png`,
-    link: "https://github.com/LaneLR/Planz-Calendar-App",
+    repoLink: "https://github.com/LaneLR/Planz-Calendar-App",
+    link: `https://planz-calendar-app.vercel.app/`, 
   },
   {
     id: "pets-adoption",
-    title: "Pets Adoption Platform",
+    title: "Pets Adoption Platform Backend",
     description: "Backend service for a pet adoption platform, built in Java.",
     image: `/images/resaleiq-portfolio-icon.png`,
-    link: "https://github.com/CoffeeCodeSyndicate/Java-Pet-Adoption-Backend",
+    repoLink:
+      "https://github.com/CoffeeCodeSyndicate/Java-Pet-Adoption-Backend",
+    link: `https://github.com/CoffeeCodeSyndicate/Java-Pet-Adoption-Backend`,
   },
   {
     id: "inventory",
     title: "Inventory Management System",
     description: "Frontend for an inventory management system.",
     image: `/images/inventoryapp.png`,
-    link: "https://github.com/katara-org/inventory-app",
+    repoLink: "https://github.com/LaneLR/inventory-app",
+    link: `https://inventory-app-lac-six.vercel.app/`,
   },
   {
     id: "news-aggregator",
     title: "News Aggregator Service",
-    description: "A web application for aggregating and displaying news from various RSS feeds.",
+    description:
+      "A web application for aggregating and displaying news from various RSS feeds.",
     image: `/images/morningfeeds.png`,
-    link: "https://github.com/LaneLR/news-aggregator-app",
+    repoLink: "https://github.com/LaneLR/news-aggregator-app",
+    link: `https://news-aggregator-app-ecru.vercel.app/`,
   },
 ];
 
@@ -59,7 +69,7 @@ export default function ProjectsComponent() {
     <div className="projects-container">
       <section className="projects-section">
         <div className="projects-section__heading-container">
-            <h2 className="projects-section__heading">Projects</h2>
+          <h2 className="projects-section__heading">Projects</h2>
         </div>
         <motion.div
           variants={container}
@@ -79,7 +89,7 @@ export default function ProjectsComponent() {
 function ProjectCard({ project }: { project: any }) {
   return (
     <motion.div variants={item} className="projects-grid__item">
-      <Link href={`${project.link}`} className="projects-grid__link">
+      <Link href={`${project.link}`} target="_blank" title="Click the image to try out the app yourself" className="projects-grid__link">
         <div className="projects-grid__image-container">
           <img
             src={project.image}
@@ -87,11 +97,13 @@ function ProjectCard({ project }: { project: any }) {
             className="projects-grid__image"
           />
         </div>
-        <div className="projects-grid__content">
-          <h3 className="projects-grid__title">{project.title}</h3>
-          <p className="projects-grid__description">{project.description}</p>
-        </div>
       </Link>
+      <div className="projects-grid__content">
+        <h3 className="projects-grid__title">
+          <Link target="_blank" href={`${project.repoLink}`} title="The title links to the repo of this project">{project.title} <LinkIcon size={14} /></Link>
+        </h3>
+        <p className="projects-grid__description">{project.description}</p>
+      </div>
     </motion.div>
   );
 }
