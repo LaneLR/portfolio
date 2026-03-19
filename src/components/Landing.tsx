794829b5c671d3ab1c79f28d76e9ea807ca9285a
LaneLR <laneleerichardson@gmail.com>
2026-03-19 14:22:12 -0500

    update landing

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -25,32 +25,30 @@ export default function LandingClient() {
         initial="hidden"
         animate="visible"
       >
-        <motion.h1 className="hero__title" variants={itemVariants}>
+<motion.h1 className="hero__title" variants={itemVariants}>
           <span>Meet</span> <span className="hero__title--accent">Lane</span>
         </motion.h1>
 
         <motion.p className="hero__subtitle" variants={itemVariants}>
           I&apos;m a{" "}
-          <strong className="hero__strong">Full-stack Software Engineer</strong>{" "}
-          specializing in building scalable frontend architectures.
-        </motion.p>
-        <motion.p className="hero__subtitle" variants={itemVariants}>
-          Currently, I&apos;m a software engineer at{" "}
-          <strong className="hero__strong">Verizon</strong> where I specialize
+          <strong className="hero__strong">Full-stack Software Engineer</strong>{" "} at {" "} 
+          <strong className="hero__strong">Verizon</strong>{" "}
+          where I specialize
           in modernizing legacy systems into React, Next.js and TypeScript, and
           enhancing user experience for large brands like{" "}
-          <strong className="hero__strong">Total Wireless</strong>,{" "}
-          <strong className="hero__strong">Straight Talk</strong>, and{" "}
-          <strong className="hero__strong">Visible</strong>.
+          <strong className="hero__strong">Total Wireless</strong>{" "}
+          {/* <strong className="hero__strong">Straight Talk</strong>,  */}
+          and <strong className="hero__strong">Visible</strong>.
         </motion.p>
+       
         <motion.p className="hero__subtitle" variants={itemVariants}>
-          I&apos;m also working on an{" "}
-          <strong className="hero__strong">
-            AI-focused mobile app for iOS
-          </strong>{" "}
-          and creating <strong className="hero__strong">agentic AI</strong>{" "}
-          agents. If you&apos;re interested or wondering about some of my other
-          work, take a look at my projects.
+          I&apos;m currently developing an{" "}
+          <strong className="hero__strong">AI-focused iOS mobile app</strong>{" "}
+          and building a custom{" "}
+          <strong className="hero__strong">AI agent</strong> using Python and
+          LangChain. By integrating Tavily for real-time web research, I&apos;m
+          building a tool that moves beyond simple chat to autonomous task
+          execution. If you are interested, feel free to explore the rest of my projects below!
         </motion.p>
 
         <motion.div className="hero__actions" variants={itemVariants}>
